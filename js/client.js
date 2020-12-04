18ccec856fa8fe6752216c1ecb280a8909649f41
William Zhang <willzhang05@gmail.com>
2020-12-04 16:54:07 +0000

    initial echo between client and ws server

---
+++ js/client.js
@@ -0,0 +1,15 @@
+const socket = new WebSocket("ws://localhost:8080/test");
+
+// Connection opened
+socket.addEventListener("open", function (event) {
+    socket.send("Hello Server!");
+});
+
+// Listen for messages
+socket.addEventListener("message", function (event) {
+    console.log("Message from server ", event.data);
+});
+
+function sendPing() {
+    socket.send("Ping!");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,21 @@
-fn main() {
+use futures::{FutureExt, StreamExt};
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+
+    let routes = warp::path("test")
+        .and(warp::ws())
+        .map(|ws: warp::ws::Ws| {
+            ws.on_upgrade(|websocket| {
+                let (tx, rx) = websocket.split();
+                rx.forward(tx).map(|result| {
+                    if let Err(e) = result {
+                        eprintln!("websocket error: {:?}", e);
+                    }
+                })
+            })
+        });
+    warp::serve(routes).run(([127, 0, 0, 1], 8080)).await;
 }
