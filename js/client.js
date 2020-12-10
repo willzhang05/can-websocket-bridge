5f7999b791416b9a6ab8b3da530ffd7766613d8d
William Zhang <willzhang05@gmail.com>
2020-12-10 17:28:31 +0000

    add connection and disconnection checks

--- js/client.js
+++ js/client.js
@@ -1,32 +1,46 @@
-var address = "localhost:8080/test";
+var url = "ws://localhost:8080/test";
 var socket;
 
 function connectToServer() {
-    socket = new WebSocket("ws://" + address);
-    // Connection opened
-    socket.addEventListener("open", function (event) {
-       console.log("Connected to", address);
-       sendCommand("hello");
-    });
-    // Listen for messages
-    socket.addEventListener("message", function (event) {
-       console.log("Message from server", event.data);
-    });
+    if (socket == null) {
+        socket = new WebSocket(url);
+        // Connection opened
+        socket.addEventListener("open", function (event) {
+           console.log("Connected to", url);
+           sendCommand("hello");
+        });
+        // Listen for messages
+        socket.addEventListener("message", function (event) {
+           console.log("Message from server", event.data);
+        });
+    } else {
+	if (socket.url == url) {
+	    if (socket.readyState == WebSocket.CONNECTING) {
+	        console.log("Already connecting!");
+	    } else if (socket.readyState == WebSocket.OPEN) {
+	        console.log("Already connected!");
+	    }
+	}
+    }
 }
 
 function disconnectFromServer() {
-    socket.close();
-    socket = null;
-    console.log("Disconnected from websocket server!");
+    if (socket == null) {
+        console.log("Not connected to anything!");
+    } else {
+        socket.close();
+        socket = null;
+        console.log("Disconnected from websocket server!");
+    }
 }
 
 
 function sendCommand(command) {
     if (socket == null) {
-	console.log("Not connected to websocket server!");
+    console.log("Not connected to websocket server!");
     } else {
         socket.send(JSON.stringify({"command": command}));
-	console.log("Sent command", command); 
+    console.log("Sent command", command); 
     }
 }
 
